c5957215729498beac798ece5b96d145d88a9245
Scott Kirkland <srkirkland@gmail.com>
2013-02-06 14:21:51 -0800

    added db management class & dept checks

---
+++ StudentService/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using StudentService.Helpers;
+using UCDArch.Web.ActionResults;
+
+namespace StudentService.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /Student/Courses?department=wxyz&key=1234
+        public ActionResult Courses(string department)
+        {
+            if (string.IsNullOrWhiteSpace(department))
+            {
+                return Json(new {error = true, errorString = "Department cannot be empty"});
+            }
+
+            using (var db = new DbManager())
+            {
+                
+            }
+
+            return new JsonNetResult();
+        }
+    }
+}
---
+++ StudentService/Helpers/DbManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace StudentService.Helpers
+{
+    public class DbManager : IDisposable
+    {
+        public readonly DbConnection Connection;
+
+        public DbManager()
+        {
+            Connection = new SqlConnection(WebConfigurationManager.ConnectionStrings[""].ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Connection.Close();
+        }
+    }
+}
