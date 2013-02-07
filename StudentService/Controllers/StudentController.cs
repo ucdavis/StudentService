061a2cf3a7b4e28ddd2d0da75c00a78a2587e4b2
Scott Kirkland <srkirkland@gmail.com>
2013-02-07 11:26:17 -0800

    cool- used query multiple to batch roster query

--- StudentService/Controllers/StudentController.cs
+++ StudentService/Controllers/StudentController.cs
@@ -17,9 +17,9 @@ namespace StudentService.Controllers
         // GET: /Student/Courses?department=wxyz&term=201301&key=1234
         public ActionResult Courses(string department, string term)
         {
-            if (string.IsNullOrWhiteSpace(department))
+            if (string.IsNullOrWhiteSpace(department) || string.IsNullOrWhiteSpace(term))
             {
-                return Json(new { error = true, errorString = "Department cannot be empty" });
+                return Json(new { error = true, errorString = "Neither Department nor Term can be empty" });
             }
 
             using (var db = new DbManager())
@@ -38,6 +38,45 @@ namespace StudentService.Controllers
                               };
                 
                 return new JsonNetResult(courses);
+            }
+        }
+
+        public ActionResult Roster(string term, string crn){
+            if (string.IsNullOrWhiteSpace(term) || string.IsNullOrWhiteSpace(crn))
+            {
+                return Json(new { error = true, errorString = "Neither Term nor Crn can be empty" });
+            }
+
+            const string query = @"select s.Pidm
+,s.FirstName
+,s.LastName
+,s.LoginId
+,s.Email
+from CourseRoster r
+	inner join students s on r.Pidm = s.Pidm
+where 
+	r.Termcode = '201301' 
+	AND r.Crn = '52960'	
+select ci.InstructorId
+,i.FirstName
+,i.Mi
+,i.LastName
+,i.LoginId
+,i.Email
+from CourseInstructors ci
+	inner join Instructors i on ci.InstructorId = i.Id
+where
+	ci.TermCode = '201301'
+	AND ci.Crn = '52960'";
+
+            using (var db = new DbManager())
+            {
+                var rosterQuery = db.Connection.QueryMultiple(query);
+
+                var students = rosterQuery.Read().ToList();
+                var instructors = rosterQuery.Read().ToList();
+
+                return new JsonNetResult();
             }
         }
     }    
