4b8cad7599c5ab29cf2170fb4fe617e49e509481
Alan Lai <anlai@ucdavis.edu>
2013-02-28 10:10:59 -0800

    updated download of terms

--- AceBanner.Db/dbo/Stored Procedures/usp_DownloadTerms.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_DownloadTerms.sql
@@ -8,7 +8,7 @@ AS
 	select * from openquery(sis, '
 		select stvterm_code, stvterm_desc, stvterm_start_date, stvterm_end_date
 		from stvterm
-		where stvterm_start_date > sysdate
+		where stvterm_end_date > sysdate
 			and stvterm_trmt_code in (''Q'', ''W'')
 	')
 
