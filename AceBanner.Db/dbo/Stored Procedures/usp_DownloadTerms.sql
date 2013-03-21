b04e868ca0e5b3352e38455e7563a8e4c42fbdbf
Alan Lai <anlai@ucdavis.edu>
2013-03-21 14:32:15 -0700

    limit terms to 2013 and later

--- AceBanner.Db/dbo/Stored Procedures/usp_DownloadTerms.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_DownloadTerms.sql
@@ -8,6 +8,7 @@ AS
 	select * from openquery(sis, '
 		select stvterm_code, stvterm_desc, stvterm_start_date, stvterm_end_date
 		from stvterm
+		where extract(year from stvterm_start_date) > 2012
 	')
 
 RETURN 0
