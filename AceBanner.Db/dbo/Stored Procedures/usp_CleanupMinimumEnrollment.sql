ed2e16e312e6d0e4398048b514388c691c6bed5b
Alan Lai <anlai@ucdavis.edu>
2013-04-10 08:16:26 -0700

    added scirpt to cleanup min enrollment #

---
+++ AceBanner.Db/dbo/Stored Procedures/usp_CleanupMinimumEnrollment.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[usp_CleanupMinimumEnrollment]
+
+AS
+
+	declare @crns table (termcode int, crn int)
+
+	insert into @crns (termcode, crn)
+	select termcode,crn from courseroster group by termcode, crn having count(pidm) < 5 
+
+	insert into @crns (termcode, crn)
+	select c.TermCode, c.Crn from Courses c left outer join CourseRoster cr on c.TermCode = cr.Termcode and c.Crn = cr.Crn where cr.pidm is null
+
+	delete ci from CourseInstructors ci
+		inner join @crns crns on ci.TermCode = crns.termcode and ci.crn = crns.crn
+
+	delete cr from CourseRoster cr
+		inner join @crns crns on cr.Termcode = crns.termcode and cr.crn = crns.crn
+
+	delete s from sections s
+		inner join @crns crns on s.Termcode = crns.termcode and s.crn = crns.crn
+
+	delete c from courses c
+	inner join @crns crns on c.Termcode = crns.termcode and c.crn = crns.crn
+
+
+RETURN 0
--- AceBanner.Db/dbo/Stored Procedures/usp_UpdateAll.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_UpdateAll.sql
@@ -8,5 +8,6 @@ exec usp_DownloadCourseRoster
 exec usp_DownloadStudents
 exec usp_DownloadInstructors
 exec usp_DownloadTerms
+exec usp_CleanupMinimumEnrollment
 
 RETURN 0
