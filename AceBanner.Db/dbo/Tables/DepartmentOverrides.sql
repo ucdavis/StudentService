129326755043b4b212e39dfa796e9d595712136e
Alan Lai <anlai@ucdavis.edu>
2013-05-02 13:28:45 -0700

    added department overrides on courses

---
+++ AceBanner.Db/dbo/Tables/DepartmentOverrides.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DepartmentOverrides]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Subject] VARCHAR(5) NOT NULL, 
+    [CourseNumb] VARCHAR(5) NULL, 
+    [DepartmentId] VARCHAR(4) NOT NULL
+)
---
+++ AceBanner.Db/dbo/Views/vCourses.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vCourses]
+	AS 
+
+select termcode, crn, [subject], coursenumb, sequence, name
+	,  isnull((
+		select top 1 do.departmentid 
+		from departmentoverrides do 
+		where (do.[subject] = courses.[subject] and do.coursenumb = courses.coursenumb)
+		   or (do.[subject] = courses.[subject] and do.coursenumb is null)
+		order by case when do.coursenumb is null then 1 else 0 end
+		), courses.departmentid) departmentid
+from courses
