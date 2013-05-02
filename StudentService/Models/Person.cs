fd77acbe0b057bb95b9783693bcc98b724d3a31c
Alan Lai <anlai@ucdavis.edu>
2013-05-02 10:45:10 -0700

    updated person to not have id

--- StudentService/Models/Person.cs
+++ StudentService/Models/Person.cs
@@ -4,14 +4,12 @@
     {
         public Person(dynamic person)
         {
-            Pidm = person.Id;
             FirstName = person.FirstName;
             LastName = person.LastName;
             LoginId = person.LoginId;
             Email = person.Email;
         }
 
-        public int Pidm { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string LoginId { get; set; }
