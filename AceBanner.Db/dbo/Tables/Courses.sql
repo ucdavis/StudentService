7e291c0e72562d952cc0d9a6c018f4e964dda9c9
Scott Kirkland <srkirkland@gmail.com>
2013-08-16 12:14:21 -0700

    clustered indexes added to courses/sections

--- AceBanner.Db/dbo/Tables/Courses.sql
+++ AceBanner.Db/dbo/Tables/Courses.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Courses] (
     [TermCode]     INT          NOT NULL,
     [Crn]          INT          NOT NULL,
-    [Subject]      VARCHAR (5) NOT NULL,
+    [Subject]      VARCHAR (5)  NOT NULL,
     [CourseNumb]   VARCHAR (5)  NOT NULL,
     [Sequence]     VARCHAR (5)  NULL,
     [Name]         VARCHAR (50) NOT NULL,
@@ -9,4 +9,11 @@
     CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED ([TermCode] ASC, [Crn] ASC),
     CONSTRAINT [FK_Courses_Courses] FOREIGN KEY ([TermCode], [Crn]) REFERENCES [dbo].[Courses] ([TermCode], [Crn])
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [Courses_TermCodeSubjectCourseNumb_IDX]
+    ON [dbo].[Courses]([TermCode] ASC, [Subject] ASC, [CourseNumb] ASC);
 
--- AceBanner.Db/dbo/Tables/Sections.sql
+++ AceBanner.Db/dbo/Tables/Sections.sql
@@ -1,13 +1,21 @@
 ﻿CREATE TABLE [dbo].[Sections] (
-	[Id] uniqueidentifier not null,
-    [TermCode]    INT         NOT NULL,
-    [Crn]         INT         NOT NULL,
-    [SectionType] CHAR (3)    NOT NULL,
-    [StartDate]   DATE        NOT NULL,
-    [EndDate]     DATE        NOT NULL,
-    [StartTime]   TIME (0)    NULL,
-    [EndTime]     TIME (0)    NULL,
-    [DaysOfWeek]  VARCHAR (5) NULL,
-    CONSTRAINT [PK_Sections] PRIMARY KEY CLUSTERED ([Id])
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [TermCode]    INT              NOT NULL,
+    [Crn]         INT              NOT NULL,
+    [SectionType] CHAR (3)         NOT NULL,
+    [StartDate]   DATE             NOT NULL,
+    [EndDate]     DATE             NOT NULL,
+    [StartTime]   TIME (0)         NULL,
+    [EndTime]     TIME (0)         NULL,
+    [DaysOfWeek]  VARCHAR (5)      NULL,
+    CONSTRAINT [PK_Sections] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [Sections_TermCode_CVIDX]
+    ON [dbo].[Sections]([TermCode] ASC)
+    INCLUDE([Crn], [SectionType], [StartDate], [EndDate], [StartTime], [EndTime], [DaysOfWeek]);
 
