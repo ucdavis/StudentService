0c28561e17ec4e2b6e19e1d10a50f9a7ab7be4a4
Scott Kirkland <srkirkland@gmail.com>
2013-08-30 13:07:21 -0700

    returns a specific department by subject

---
+++ StudentService/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using StudentService.Helpers;
+using UCDArch.Web.ActionResults;
+
+namespace StudentService.Controllers
+{
+    [AllowOrigin]
+    [ValidateKey]
+    public class DepartmentController : Controller
+    {
+        // GET: /Course/List?department=wxyz&term=201301&key=1234
+        public ActionResult Find(string subject)
+        {
+            if (string.IsNullOrWhiteSpace(subject))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Subject cannot be empty");
+            }
+
+            using (var db = new DbManager())
+            {
+                var departments = db.Connection.Query(QueryResources.DeparmentFindQuery, new { id = subject });
+                return new JsonNetResult(departments.FirstOrDefault());
+            }
+        }
+
+
+    }
+}
