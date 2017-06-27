9db25111efe9c1d6bbeacb01a02b86e1082b366e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-27 10:38:25 -0700

    remove constructors

--- StudentService/Models/Student.cs
+++ StudentService/Models/Student.cs
@@ -4,40 +4,6 @@ namespace StudentService.Models
 {
     public class Student
     {
-        public Student(dynamic student)
-        {
-            Pidm = student.pidm;
-            StudentId = student.studentId;
-            FirstName = student.firstName;
-            Mi = student.mi;
-            LastName = student.lastName;
-            Email = student.email;
-            EarnedUnits = student.earnedUnits;
-            CurrentUnits = student.currentUnits;
-            Major = student.major;
-            LastTerm = student.lastTerm;
-            Astd = student.astd;
-            LoginId = student.loginId;
-            Sja = student.sja;
-        }
-
-        public Student(SqlDataReader reader)
-        {
-            Pidm = reader[0] as string;
-            StudentId = reader[1] as string;
-            FirstName = reader[2] as string;
-            Mi = reader[3] as string;
-            LastName = reader[4] as string;
-            Email = reader[5] as string;
-            EarnedUnits = (decimal)reader[6];
-            CurrentUnits = (decimal)reader[7];
-            Major = reader[8] as string;
-            LastTerm = reader[9] as string;
-            Astd = reader[10] as string;
-            LoginId = reader[11] as string;
-            Sja = false;
-        }
-
         public string Pidm { get; set; }
         public string StudentId  { get; set; }
         public string FirstName { get; set; }
