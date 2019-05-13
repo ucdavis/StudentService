754000324c44b86fec190851bab089bf72946c58
Scott Kirkland <srkirkland@gmail.com>
2019-05-13 12:00:46 -0700

    braces

--- AceBanner.Db/dbo/Stored Procedures/usp_DownloadCourses.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_DownloadCourses.sql
@@ -47,7 +47,7 @@ BEGIN
 			) course on ssbsect_subj_code = scbcrse_subj_code and ssbsect_crse_numb = scbcrse_crse_numb
 		where ssbsect_term_code in ( select stvterm_code from stvterm
 									where (stvterm_start_date < sysdate + 21 and stvterm_end_date > sysdate)
-									   or (stvterm_start_date < sysdate and stvterm_end_date > (sysdate - 21))
+									   or (stvterm_start_date < sysdate and stvterm_end_date > sysdate - 21)
 								  )
 		  and course.scbcrse_dept_code is not null
 	')
