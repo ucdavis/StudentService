2301b9aca6e15a0bed02318177c2c0a6b871a6e5
Ken Taylor <kentaylor@ucdavis.edu>
2019-05-14 13:06:54 -0700

    Revised to use dynamic SQL and
    new udf_TermCodeFilterString .

--- AceBanner.Db/dbo/Stored Procedures/usp_DownloadCourses.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_DownloadCourses.sql
@@ -3,21 +3,38 @@
 -- Create date: 
 -- Description:	Download all courses for the active term, or recently closed term.  
 -- If the term has opened in the last 1 days, don't yet download course information (sysdate - 1).
+-- Usage:
+/*
+
+USE StudentService
+	GO
+
+	EXEC [dbo].[usp_DownloadCourses] @IsDebug = 1
+	GO
+
+*/
+-- Modifications:
+--	2019-05-14 by kjt: Converted to dynamic SQL so that universal term code portion of where clause
+--	  could be pulled from udf_TermCodeFilterString and used by all stored procedures requiring it.
+--
 -- =============================================
 CREATE PROCEDURE [dbo].[usp_DownloadCourses]
-	
+	( @IsDebug bit = 0)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
+	DECLARE @TSQL varchar(MAX) = ''
+
+	SELECT @TSQL = '
 	truncate table Courses
 
     insert into Courses
 	select distinct ssbsect_term_code, ssbsect_crn, ssbsect_subj_code, ssbsect_crse_numb
 		, ssbsect_seq_numb, scbcrse_title, scbcrse_dept_code, students_needing_grades, ssbsect_gradable_ind
-	from openquery (sis, '
+	from openquery (sis, ''
 		select ssbsect_term_code
 			, ssbsect_crn
 			, ssbsect_subj_code
@@ -30,9 +47,9 @@ BEGIN
   				from sfrstcr
 					where sfrstcr_crn = ssbsect_crn 
 						and sfrstcr_term_code = ssbsect_term_code
-						and sfrstcr_rsts_code =''RE''
+						and sfrstcr_rsts_code =''''RE''''
 						and sfrstcr_grde_code is null
-						and ssbsect_gradable_ind = ''Y''
+						and ssbsect_gradable_ind = ''''Y''''
 			) students_needing_grades
 			, ssbsect_gradable_ind
 		from ssbsect 
@@ -45,11 +62,17 @@ BEGIN
 						group by scbcrse_subj_code, scbcrse_crse_numb
 					) maxscb on SCBCRSE_SUBJ_CODE = maxscb.subj and SCBCRSE_CRSE_NUMB = maxscb.crse and SCBCRSE_EFF_TERM = maxscb.term
 			) course on ssbsect_subj_code = scbcrse_subj_code and ssbsect_crse_numb = scbcrse_crse_numb
-		where ssbsect_term_code in ( select stvterm_code from stvterm
-									where (stvterm_start_date < (sysdate - 1) and stvterm_end_date > sysdate)
-									   or (stvterm_start_date < (sysdate - 1) and stvterm_end_date > (sysdate - 21))
-								  )
+		where ssbsect_term_code in (' 
+
+	SELECT @TSQL += (SELECT [dbo].[udf_TermCodeFilterString]())
+
+	SELECT @TSQL += '			 )
 		  and course.scbcrse_dept_code is not null
-	')
+	'')
+'
+	IF @IsDebug = 1
+		PRINT @TSQL
+	ELSE
+		EXEC (@TSQL)
 
 END
--- AceBanner.Db/dbo/Stored Procedures/usp_DownloadStudents.sql
+++ AceBanner.Db/dbo/Stored Procedures/usp_DownloadStudents.sql
@@ -1,21 +1,36 @@
 ﻿-- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
+-- Author:		Alan Lai
+-- Create date: Unknown
+-- Description:	Download students for student service.
+-- Usage:
+/*
+	USE StudentService
+	GO
+
+	EXEC [dbo].[usp_DownloadStudents] @IsDebug = 1
+	GO
+*/
+-- Modifications:
+--	2019-05-14 by kjt: Split out term code where clause portion so that it might be
+--	used by all five of the download stored procedures.
+--
 -- =============================================
 CREATE PROCEDURE [dbo].[usp_DownloadStudents]
-	
+	( @IsDebug bit = 0)
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
+	DECLARE @TSQL varchar(MAX) = ''
+
+	SELECT @TSQL = '
     truncate table Students
 
 	insert into Students
-	select loginid, firstname, lastname, pidm, studentid, email, 'B'
-	from openquery (sis, '
+	select loginid, firstname, lastname, pidm, studentid, email, ''B''
+	from openquery (sis, ''
 		select lower(wormoth_login_id) as loginid 
 			, spriden_first_name as firstname, spriden_last_name as lastname, spriden_pidm as pidm, spriden_id as studentid
 			, emails.address as email
@@ -24,22 +39,28 @@ BEGIN
 			left outer join (
 				select goremal_pidm as pidm, goremal_email_address as address
 				from goremal
-				where goremal_status_ind = ''A''
-					and goremal_emal_code = ''UCD''
+				where goremal_status_ind = ''''A''''
+					and goremal_emal_code = ''''UCD''''
 			) Emails on Emails.pidm = spriden_pidm
 		where spriden_change_ind is null
 		  and spriden_pidm in (
 				select sfrstcr_pidm
 				from sfrstcr
-				where sfrstcr_term_code in ( select stvterm_code from stvterm
-											where (stvterm_start_date < sysdate and stvterm_end_date > sysdate)
-											   or (stvterm_start_date < sysdate and stvterm_end_date > sysdate - 21)
-										  )
-				  and sfrstcr_rsts_code in (''RE'', ''RW'')
+				where sfrstcr_term_code in (' 
+
+	SELECT @TSQL += (SELECT [dbo].[udf_TermCodeFilterString]())
+
+	SELECT @TSQL += '			 )
+				  and sfrstcr_rsts_code in (''''RE'''', ''''RW'''')
 		  )
-		  and wormoth_acct_type = ''Z''
-		  and wormoth_acct_status = ''A''
-		  and WORMOTH_ACTIVITY_DATE = ( select max(wormoth_activity_date ) from wormoth iw where iw.WORMOTH_PIDM = wormoth.wormoth_pidm and iw.wormoth_acct_type = ''Z'' and iw.wormoth_acct_status = ''A'' )
-	')
+		  and wormoth_acct_type = ''''Z''''
+		  and wormoth_acct_status = ''''A''''
+		  and WORMOTH_ACTIVITY_DATE = ( select max(wormoth_activity_date ) from wormoth iw where iw.WORMOTH_PIDM = wormoth.wormoth_pidm and iw.wormoth_acct_type = ''''Z'''' and iw.wormoth_acct_status = ''''A'''' )
+	'')
+'
+	IF @IsDebug = 1
+		PRINT @TSQL
+	ELSE
+		EXEC (@TSQL)
 
 END
